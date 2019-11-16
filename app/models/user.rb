24770605ecef9d5f4d68cf0d1082b2db12b123b6
Bok Enciso <enciso.arnulfo@gmail.com>
2019-11-16 09:01:42 +0800

    Creating Teams Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :teams
+
+  #View Helpers
+  def user_one
+    self.users.first.first_name
+  end
+
+  def team_player_two
+    self.users.last.first_name unless self.users.first.first_name == self.users.last.first_name
+  end
 end
