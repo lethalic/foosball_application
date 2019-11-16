9e5274a11b9571d81c8af15079bd304aa6a5ad35
Bok Enciso <enciso.arnulfo@gmail.com>
2019-11-16 08:54:41 +0800

    Creating Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+  get '/ranking' => 'home#ranking'
+
+  resources :users
+  resources :teams
+  resources :matches
+
+  get "*any", via: :all, to: "errors#not_found"
 end
